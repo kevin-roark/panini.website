4bd6fc5653a1cbd720b599e3f64a3850dfa1ec66
Kevin Roark <ker2143@columbia.edu>
2015-04-04 02:12:27 -0400

    step climbing looking pretty nice

--- climb/climb.js
+++ climb/climb.js
@@ -1,17 +1,19 @@
 
-var STEP_HEIGHT = 50;
-var STEP_LENGTH = 80;
-var MIN_COUNT = 50;
+var STEP_PADDING = 10;
+var ME_PADDING = 20;
+var STEP_HEIGHT = Math.round((view.size.height - STEP_PADDING * 2) / 16);
+var STEP_LENGTH = Math.round((view.size.width - STEP_PADDING * 2) / 16);
+console.log('length: ' + STEP_LENGTH + ' / height: ' + STEP_HEIGHT);
 
 drawSteps();
 function drawSteps() {
   var path = new Path();
   path.strokeColor = 'black';
 
-  var point = new Point(10, view.size.height - 10);
+  var point = new Point(STEP_PADDING, view.size.height - STEP_PADDING);
   path.moveTo(point);
 
-  for (var i = 0; i < 20; i++) {
+  for (var i = 0; i < 16; i++) {
     point = point + [STEP_LENGTH, 0];
     path.lineTo(point);
     point = point + [0, -STEP_HEIGHT];
@@ -19,49 +21,63 @@ function drawSteps() {
   }
 }
 
-var me = new Path.Circle(new Point(10, view.size.height - 30), 5);
+var me = new Path.Circle(new Point(ME_PADDING, view.size.height - ME_PADDING), 5);
 me.fillColor = 'black';
 
-var movingHorizontally = true;
-var nextHorizontalMilestone = STEP_LENGTH;
-var nextVerticalMilestone = view.size.height - 30 - STEP_HEIGHT;
+var nextForwardMove = 'x';
+var nextBackwardMove = 'x';
 
-function iterate(count) {
-  // if (count < MIN_COUNT) return;
-  //
-  // if (movingHorizontally) {
-  //   me.position += [1, 0];
-  //   if (me.position.x == nextHorizontalMilestone) {
-  //     movingHorizontally = false;
-  //     nextHorizontalMilestone += STEP_LENGTH;
-  //   }
-  // }
-  // else {
-  //   me.position += [0, -1];
-  //   if (me.position.y == nextVerticalMilestone) {
-  //     movingHorizontally = true;
-  //     nextVerticalMilestone -= STEP_HEIGHT;
-  //   }
-  // }
+function iterate(forward) {
+  if (forward) {
+    if (nextForwardMove === 'x') {
+      me.position.x += 1;
+      if (me.position.x % STEP_LENGTH === 0) {
+        nextForwardMove = 'y';
+      }
+      else {
+        nextBackwardMove = 'x';
+      }
+    }
+    else {
+      me.position.y -= 1;
+      if ((view.size.height - STEP_PADDING * 2 - me.position.y) % STEP_HEIGHT === 0) {
+        nextForwardMove = 'x';
+      }
+      else {
+        nextBackwardMove = 'y';
+      }
+    }
+  }
+  else {
+    if (nextBackwardMove === 'x') {
+      me.position.x -= 1;
+      if (me.position.x % STEP_LENGTH === 0) {
+        nextBackwardMove = 'y';
+      }
+      else {
+        nextForwardMove = 'x';
+      }
+    }
+    else {
+      me.position.y += 1;
+      if ((view.size.height - STEP_PADDING * 2 - me.position.y) % STEP_HEIGHT === 0) {
+        nextBackwardMove = 'x';
+      }
+      else {
+        nextForwardMove = 'y';
+      }
+    }
+  }
 }
 
 function onFrame(event) {
-  iterate(event.count);
+
 }
 
 function onKeyDown(event) {
-  switch (event.key) {
-    case 'up':
-      me.position += [0, -1];
-      break;
-    case 'down':
-      me.position += [0, 1];
-      break;
-    case 'left':
-      me.position += [-1, 0];
-      break;
-    case 'right':
-      me.position += [1, 0];
-      break;
+  if (event.key == 'right') {
+    iterate(true);
+  } else if (event.key == 'left') {
+    iterate(false);
   }
 }
